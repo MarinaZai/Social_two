718c3cd7c5a05e6e2b57d410655a1e62e20dbcb7
MarinaZai <Marintos34@gmail.com>
2022-06-27 12:39:41 +0300

    reselect

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,53 +1,46 @@
-import React from 'react';
+import React from "react";
 import { Form, Field } from "react-final-form";
-import s from '../MyPosts/MyPosts.module.css'
-import { Post } from '../Post/Post';
+import s from "../MyPosts/MyPosts.module.css";
+import { Post } from "../Post/Post";
 
 export const MyPosts = (props) => {
-    let postsElements = props.posts.map(
-        post => (<Post key={post.id} message={post.message} likes={post.likes} />)
-    )
+  let postsElements = props.posts.map((post) => (
+    <Post key={post.id} message={post.message} likes={post.likes} />
+  ));
 
-    let newPostElement = React.createRef();
-    let onAddPost = (values) => {
-        props.addPost(values.newPostText);
-    }
-    return (
-        <div className={s.postsBlock}>
-            <h3>MyPosts</h3>
-  {/*           <div className={s.NewPosts}>
-                <div>
-                    <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText} />
-                </div>
-                <div>
-                    <button onClick={onAddPost}>Add Post</button>
-                </div>
-            </div> */}
-            <AddNewPostForm onSubmit={onAddPost}  />
-            <div className={s.posts}>
-                {postsElements}
-            </div>
-        </div>)
-}
-
-export const AddNewPostForm = (props) => {
-    return (
-      <Form
-          onSubmit={props.onSubmit} 
-        render={({ handleSubmit }) => (
-          <form onSubmit={handleSubmit} className={s.NewMessage}>
-            <div>
-              <Field
-                name="newPostText"
-                component="textarea"
-                placeholder="Enter your message"
-              />
-            </div>
-            <div>
-              <button>Add Message</button>
-            </div>
-          </form>
-        )}
-      />
-    );
+  let newPostElement = React.createRef();
+  let onAddPost = (values) => {
+    props.addPost(values.newPostText);
   };
+  return (
+    <div className={s.postsBlock}>
+      <h3>MyPosts</h3>
+      <AddNewPostForm onSubmit={onAddPost} />
+      <div className={s.posts}>{postsElements}</div>
+    </div>
+  );
+};
+
+
+///
+export const AddNewPostForm = (props) => {
+  return (
+    <Form
+      onSubmit={props.onSubmit}
+      render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit} className={s.NewMessage}>
+          <div>
+            <Field
+              name="newPostText"
+              component="textarea"
+              placeholder="Enter your message"
+            />
+          </div>
+          <div>
+            <button>Add Message</button>
+          </div>
+        </form>
+      )}
+    />
+  );
+};
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import { connect } from "react-redux";
 import {
   follow,
-  getUsers,
+  requestUsers,
   setCurrentPage,
   toggleFollowingProgress,
   unfollow
 } from "../../redux/users-reducer";
 import { Users } from "./Users";
 import { Preloader } from "../common/Preloader/Preloader";
+import { getCurrentPage, getFollowingInProgress, getIsFetching, getPageSize, getTotalUsersCount, getUsers} from "../../redux/users-selectors";
 
 
 class UsersContainer extends React.Component {
   componentDidMount() {
-    this.props.getUsers(this.props.currentPage, this.props.pageSize);
+    this.props.requestUsers(this.props.currentPage, this.props.pageSize);
   }
 
   onPageChanged = (pageNumber) => {
       this.props.setCurrentPage(pageNumber)
-    this.props.getUsers(pageNumber, this.props.pageSize);
+    this.props.requestUsers(pageNumber, this.props.pageSize);
   };
 
   render() {
@@ -40,7 +41,7 @@ class UsersContainer extends React.Component {
   }
 }
 
-let mapStateToProps = (state) => {
+/* let mapStateToProps = (state) => {
   return {
     users: state.usersPage.users,
     pageSize: state.usersPage.pageSize,
@@ -49,6 +50,18 @@ let mapStateToProps = (state) => {
     isFetching: state.usersPage.isFetching,
     followingInProgress: state.usersPage.followingInProgress
   };
+}; */
+
+let mapStateToProps = (state) => {
+  return {
+    users: getUsers(state),
+    //users: getUsers(state),
+    pageSize: getPageSize(state),
+    totalUsersCount: getTotalUsersCount(state),
+    currentPage: getCurrentPage(state),
+    isFetching: getIsFetching(state),
+    followingInProgress: getFollowingInProgress(state)
+  };
 };
 
 export default connect(mapStateToProps, {
@@ -56,5 +69,5 @@ export default connect(mapStateToProps, {
   unfollow,
   setCurrentPage,
   toggleFollowingProgress,
-  getUsers
+  requestUsers
 })(UsersContainer);
